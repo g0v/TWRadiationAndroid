11ab77932acc2e18b1af49d11537eb7b212f7f0e
Julian_Chu <walkingice@0xlab.org>
2014-10-04 16:53:24 +0800

    add basic navigation drawer

--- src/tw/radiation/MainActivity.java
+++ src/tw/radiation/MainActivity.java
@@ -1,13 +1,87 @@
 package tw.radiation;
 
+import tw.radiation.R;
+
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+    private DrawerLayout mLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView mDrawerList;
+
+    private CharSequence mTitle;
+    private String[] mNames;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.main_activity);
+        mTitle = getTitle();
+        mLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList =  (ListView) findViewById(R.id.left_menu);
+
+        mNames = new String[2];
+        mNames[0] = getString(R.string.list_name_main);
+        mNames[1] = getString(R.string.list_name_about);
+
+        mDrawerList.setAdapter(new ArrayAdapter<CharSequence>(this,
+                    R.layout.drawer_list_item, mNames));
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mLayout,
+                R.drawable.ic_drawer,
+                R.string.drawer_open,
+                R.string.drawer_close
+                ){
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getActionBar().setTitle(mTitle);
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getActionBar().setTitle(mTitle);
+            }
+        };
+
+        mLayout.setDrawerListener(mDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
 
