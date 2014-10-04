9ffbfdf129fa84a0075f8ecc3c33a70a44821468
Julian_Chu <walkingice@0xlab.org>
2014-10-04 14:19:11 +0800

    init empty android app

---
+++ src/tw/radiation/MainActivity.java
@@ -0,0 +1,13 @@
+package tw.radiation;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
